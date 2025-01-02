ad5ae1ce5951768b221b8cd2cb65319bbaf6f608
Alex Cabrera <uy.acabrera@gmail.com>
2025-01-01 21:29:31 -0300

    Added href

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import { Projects } from './pages/Projects';
 function App() {
   return (
     <ThemeProvider>
-      <Router>
+      <Router basename="Portfolio">
         <div className="min-h-screen flex flex-col bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
           <Header />
           <main className="flex-grow">
